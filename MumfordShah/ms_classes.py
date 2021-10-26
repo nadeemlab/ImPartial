2f33d6d058356588ecd45fced0e53df7bb37a908
Gunjan Shrivastava <gunjan.sh123@gmail.com>
2021-10-26 13:03:19 -0400

    path generalization for MumfordShah model

--- MumfordShah/ms_classes.py
+++ MumfordShah/ms_classes.py
@@ -1,21 +1,24 @@
 import argparse
+import os
 import sys
-sys.path.append("../")
-from general.utils import save_json
+import pandas as pd
 import torch
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
-import pandas as pd
+
+sys.path.append("../")
+
+from general.utils import save_json
 from general.utils import model_params_load , mkdir, save_json, to_np
 from MumfordShah.ms_functions import get_ms_outputs
 from dataprocessing.dataloaders import Normalize, ToTensor, ImageSegDataset,ImageBlindSpotDataset, RandomFlip
 from torchvision import transforms
 from general.evaluation import get_performance
-import os
+
 class MumfordShahConfig(argparse.Namespace):
 
-    def __init__(self,config_dic = None,**kwargs):
+    def __init__(self, config_dic=None, **kwargs):
 
         self.basedir = 'models/'
         self.model_name = 'vanilla_model'
@@ -137,7 +140,7 @@ class MumfordShahConfig(argparse.Namespace):
             setattr(self, k, kwargs[k])
 
 
-    def save_json(self,save_path=None):
+    def save_json(self, save_path=None):
         config_dict = self.__dict__
         config2json = {}
         for key in config_dict.keys():
@@ -152,7 +155,8 @@ class MumfordShahConfig(argparse.Namespace):
                 else:
                     config2json[key] = config_dict[key]
         if save_path is None:
-            save_path =  self.basedir + self.model_name + '/config.json'
+            #save_path =  self.basedir + self.model_name + '/config.json'    # todo gs
+            save_path =  os.path.join(self.basedir, self.model_name, 'config.json')    # todo gs
         save_json(config2json, save_path)
         print('Saving config json file in : ', save_path)
 
@@ -189,9 +193,10 @@ class MumfordShahModel:
             else:
                 self.optimizer = optim.SGD(self.model.parameters(), lr=config.LEARNING_RATE)
 
+        self.model_output_dir = os.path.join(self.config.basedir, self.config.model_name)
         print('---------------- Mumford Shah model config created ----------------------------')
         print()
-        print('Model directory:', self.config.basedir + self.config.model_name + '/')
+        print('Model directory:', self.model_output_dir)   # todo gs
         print()
         print('-- Config file :')
         print(self.config)
@@ -206,7 +211,7 @@ class MumfordShahModel:
         print()
         print()
         mkdir(self.config.basedir)
-        mkdir(self.config.basedir+self.config.model_name+'/')
+        mkdir(self.model_output_dir)           # todo gs
 
         self.dataloader_train = None
         self.dataloader_val = None
@@ -218,7 +223,7 @@ class MumfordShahModel:
             print(' Loading : ', load_file)
             model_params_load(load_file, self.model, self.optimizer,self.config.DEVICE)
 
-    def load_dataloaders(self,pd_files_scribbles):
+    def load_dataloaders(self, data_dir, pd_files_scribbles):
 
         # ------------------------- Dataloaders --------------------------------#
         print('-- Dataloaders : ')
@@ -232,8 +237,8 @@ class MumfordShahModel:
 
         transform_train = transforms.Compose(transforms_list)
 
-        # dataaset train
-        dataset_train = ImageBlindSpotDataset(pd_files_scribbles, transform=transform_train, validation=False,
+        # dataaset train    # todo gs
+        dataset_train = ImageBlindSpotDataset(pd_files_scribbles, data_dir, transform=transform_train, validation=False,  # todo gs
                                               ratio=self.config.ratio, size_window=self.config.size_window,
                                               p_scribble_crop=self.config.p_scribble_crop, shift_crop=self.config.shift_crop,
                                               patch_size=self.config.patch_size, npatch_image=self.config.npatch_image_sampler)
@@ -249,8 +254,8 @@ class MumfordShahModel:
         transforms_list.append(ToTensor())
         transform_eval = transforms.Compose(transforms_list)
 
-        # dataset validation
-        dataset_val = ImageBlindSpotDataset(pd_files_scribbles, transform=transform_eval, validation=True,
+        # dataset validation    # todo gs
+        dataset_val = ImageBlindSpotDataset(pd_files_scribbles, data_dir, transform=transform_eval, validation=True,      # todo gs
                                             ratio=1, size_window=self.config.size_window,
                                             p_scribble_crop=self.config.p_scribble_crop, shift_crop=self.config.shift_crop,
                                             patch_size=self.config.patch_size, npatch_image=self.config.npatch_image_sampler)
@@ -287,20 +292,20 @@ class MumfordShahModel:
                 history[key] = np.array(history[key]).tolist()
 
             from general.utils import save_json
-            save_json(history, self.config.basedir + self.config.model_name + '/history.json')
-            print('history file saved on : ', self.config.basedir + self.config.model_name + '/history.json')
+            _path = os.path.join(self.model_output_dir, 'history.json')
+            save_json(history, _path)                  # todo gs
+            print('history file saved on : ', _path)   
 
             return history
         else:
             print('No train/val dataloader was loaded')
 
-    def eval(self, pd_files, default_ensembles=True, model_ensemble_load_files=[]):
+    def eval(self, pd_files, data_dir, default_ensembles=True, model_ensemble_load_files=[]):
 
         if default_ensembles & (len(model_ensemble_load_files) < 1):
             model_ensemble_load_files = []
             for model_file in self.config.val_model_saves_list:
-                model_ensemble_load_files.append(
-                    self.config.basedir + self.config.model_name + '/' + model_file)
+                model_ensemble_load_files.append(os.path.join(self.model_output_dir, model_file))
 
         if len(model_ensemble_load_files) >= 1:
             print('Evaluating average predictions of models : ')
@@ -318,7 +323,7 @@ class MumfordShahModel:
 
         # dataloader full images evaluation
         batch_size = 1
-        dataloader_eval = torch.utils.data.DataLoader(ImageSegDataset(pd_files, transform=transform_eval),
+        dataloader_eval = torch.utils.data.DataLoader(ImageSegDataset(pd_files, data_dir, transform=transform_eval),
                                                       batch_size=batch_size, shuffle=False, num_workers=8) ## Batch size 1 !!!
 
         # ---------- Evaluation --------------#
@@ -362,10 +367,10 @@ class MumfordShahModel:
             output_list.append(output)
         return output_list, gt_list
 
-    def data_performance_evaluation(self, pd_files, saveout=False, plot=False, default_ensembles=True,
+    def data_performance_evaluation(self, pd_files, data_dir, saveout=False, plot=False, default_ensembles=True,  # todo gs
                                     model_ensemble_load_files=[]):
 
-        output_list, gt_list = self.eval(pd_files, default_ensembles=default_ensembles,
+        output_list, gt_list = self.eval(pd_files, data_dir, default_ensembles=default_ensembles,     # todo gs
                                          model_ensemble_load_files=model_ensemble_load_files)
 
         th_list = np.linspace(0, 1, 21)[1:-1]
@@ -413,11 +418,13 @@ class MumfordShahModel:
             ## Save output (optional)
             if saveout:
                 prefix = pd_files.iloc[ix_file]['prefix']
-                save_output_dic = self.config.basedir + self.config.model_name + '/output_images/'
+                # save_output_dic = self.config.basedir + self.config.model_name + '/output_images/'      # todo gs
+                save_output_dic = os.path.join(self.model_output_dir, 'output_images')
                 file_output_save = 'eval_' + prefix + '.pickle'
                 mkdir(save_output_dic)
-                print('Saving output : ',save_output_dic + file_output_save)
-                with open(save_output_dic + file_output_save, 'wb') as handle:
+                _path = os.path.join(save_output_dic, file_output_save)
+                print('Saving output : ', _path)
+                with open(_path, 'wb') as handle:
                     pickle.dump(output, handle)
                 pd_saves_out.append([prefix, file_output_save])
 
@@ -429,7 +436,8 @@ class MumfordShahModel:
 
         if saveout:
             pd_saves = pd.DataFrame(data=pd_saves_out, columns=['prefix', 'output_file'])
-            pd_saves.to_csv(save_output_dic + 'pd_output_saves.csv', index=0)
-            print('pandas outputs file saved in :', save_output_dic + 'pd_output_saves.csv')
+            _path = os.path.join(save_output_dic, 'pd_output_saves.csv')    # todo gs
+            pd_saves.to_csv(_path, index=0)
+            print('pandas outputs file saved in :', _path)
 
         return pd_summary
