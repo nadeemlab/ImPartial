170a7bfafea39fbcbbb04fdbeee699fff147943c
Grigory Frantsuzov <grigoriy.frantsuzov@quantori.com>
2024-03-27 17:29:13 +0300

    Added logs and mutex to fix the issue

--- imagej-plugin/src/main/java/org/nadeemlab/impartial/BaseApiClient.java
+++ imagej-plugin/src/main/java/org/nadeemlab/impartial/BaseApiClient.java
@@ -1,8 +1,7 @@
 package org.nadeemlab.impartial;
 
 import okhttp3.*;
-import org.json.JSONException;
-import org.json.JSONObject;
+import okio.Buffer;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
@@ -55,22 +54,22 @@ public class BaseApiClient {
                 .build();
     }
 
-    protected void raiseForStatus(Response res) throws IOException {
-        if (!res.isSuccessful()) {
-            String message = res.message();
-            String description = "";
-            if (res.body() != null && res.body().contentLength() > 0) {
-                try {
-                    JSONObject jsonRes = new JSONObject(res.body().string());
-                    if (jsonRes.has("message"))
-                        message = jsonRes.getString("message");
-                    description = jsonRes.has("description") ?
-                            jsonRes.getString("description") : jsonRes.getString("detail");
-                } catch (JSONException ignore) {}
+    protected void raiseForStatus(Response response) throws IOException {
+        logResponseInfo(response);
+
+        if (!response.isSuccessful()) {
+            String responseBody = "";
+
+            if (response.body() != null && response.body().contentLength() > 0) {
+                responseBody = response.body().string();
             }
 
+            String error = getErrorLog(response, responseBody);
+
+            System.out.println(error);
+
             throw new IOException(
-                    String.format("%d %s \n%s", res.code(), message, description
+                    String.format("%d %s \n%s", response.code(), "Error message: ", error
                     ));
         }
     }
@@ -82,5 +81,59 @@ public class BaseApiClient {
 
     protected Request.Builder getRequestBuilder() {
         return new Request.Builder();
+    }
+
+    protected Response callHttpClientAndLogRequestInfo(Request request) throws IOException {
+        System.out.println("=========================<HTTP QUERY INFO>===================================");
+        String requestMethod = request.method();
+        System.out.println("Request method: " + requestMethod);
+        System.out.println("Request url: " + request.url());
+        System.out.println("Request headers: " + request.headers());
+
+        if (request.body() != null && request.body().contentLength() > 0) {
+            System.out.println("Request body: " + request.body().toString());
+            System.out.println("Request body: " + bodyToString(request));
+        }
+
+        return httpClient.newCall(request).execute();
+    }
+
+    private void logResponseInfo(Response response) {
+        int responseCode = response.code();
+        System.out.println("Response for url: " + response.request().url());
+        System.out.println("Response code: " + responseCode);
+        System.out.println("=========================</HTTP QUERY INFO>===================================");
+    }
+
+    private String getErrorLog(Response response, String responseBody) throws IOException {
+        int responseCode = response.code();
+        String requestMethod = response.request().method();
+
+        String requestBody = null;
+        if (response.request().body() != null && response.request().body().contentLength() > 0) {
+            requestBody = response.request().body().toString();
+        }
+
+        return "++++++++++++++++++++++++++++++++<ERROR LOG>++++++++++++++++++++++++++++++++++++++" + "\n" +
+                "Response for url: " + response.request().url() + "\n" +
+                "Request method: " + requestMethod + "\n" +
+                "Request headers: " + response.request().headers() + "\n" +
+                "Request body: " + requestBody + "\n" +
+                "Request body: " + bodyToString(response.request()) + "\n" +
+                "~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                "Response headers: " + response.headers() + "\n" +
+                "Response code: " + responseCode + "\n" +
+                "Response body: " + responseBody + "\n" +
+                "++++++++++++++++++++++++++++++++</ERROR LOG>++++++++++++++++++++++++++++++++++++++" + "\n";
+    }
+
+    private static String bodyToString(final Request request) {
+        try (Buffer buffer = new Buffer()) {
+            final Request copy = request.newBuilder().build();
+            copy.body().writeTo(buffer);
+            return buffer.readUtf8();
+        } catch (final IOException e) {
+            return "Body could not be parsed";
+        }
     }
 }
