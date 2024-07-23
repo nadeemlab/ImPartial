838f1fc04e20b4754467dc2c5693181b94607228
Gunjan Shrivastava <gunjan.sh123@gmail.com>
2024-07-22 22:01:18 -0400

    simplify losses.py

--- general/losses.py
+++ general/losses.py
@@ -8,6 +8,184 @@ from sklearn.metrics import roc_auc_score
 sys.path.append("../")
 from .utils import to_np
 
+
+import sys
+import numpy as np
+from scipy.special import softmax
+import torch
+
+class LossConfig():
+    def __init__(self, device):
+        self.device = device
+        self.rec_loss = 'gaussian'
+        self.reg_loss = 'L1'
+
+        self.weight_tasks = None #per segmentation class reconstruction weight
+        classification_tasks = {'0': {'classes': 1, 'rec_channels': [0], 'ncomponents': [1,2]}}  # list containing classes 'object types'
+        for key in classification_tasks.keys():
+            nrec = len(classification_tasks[key]['rec_channels'])
+            nclasses = classification_tasks[key]['classes']
+
+            if 'weight_classes' not in classification_tasks[key].keys():
+                classification_tasks[key]['weight_classes'] = [1/nclasses for _ in range(nclasses)]
+            if 'weight_rec_channels' not in classification_tasks[key].keys():
+                classification_tasks[key]['weight_rec_channels'] = [1/nrec for _ in range(nrec)]
+
+        if self.weight_tasks is None:
+            self.weight_tasks = {}
+            for key in classification_tasks.keys():
+                self.weight_tasks[key] = 1/len(classification_tasks.keys())
+
+        self.classification_tasks = classification_tasks
+
+        self.mean = True
+        self.std = False
+        self.weight_objectives = {'seg_fore': 0.45, 'seg_back': 0.45, 'rec': 0.1, 'reg': 0.0}
+
+
+class ImpartialLoss():
+    def __init__(self, loss_config):
+        self.config = loss_config
+
+        self.criterio_seg = None
+        self.criterio_rec = None 
+        self.criterio_reg = None
+
+        self.criterio_seg = seglosses(type_loss='CE', reduction=None)
+
+        if 'reg' in self.config.weight_objectives.keys():
+            if self.config.weight_objectives['reg'] > 0:
+                self.criterio_reg = gradientLoss2d(penalty=self.config.reg_loss, reduction='mean')
+
+        if 'rec' in loss_config.weight_objectives.keys():
+            if self.config.weight_objectives['rec'] > 0:
+                self.criterio_rec = reclosses(type_loss=self.config.rec_loss, reduction=None)
+                
+
+    def compute_loss(self, out, input, scribble, mask):
+
+        rec_loss_dic = {}
+        seg_fore_loss_dic = {}
+        seg_back_loss_dic = {}
+        reg_loss_dic = {}
+
+        total_loss = {}
+        total_loss['rec'] = 0
+        total_loss['seg_fore'] = 0
+        total_loss['seg_back'] = 0
+        total_loss['reg'] = 0
+
+        ix = 0 #out channel index
+        ix_scribbles = 0 #scribbles index
+        for class_tasks_key in self.config.classification_tasks.keys():
+            classification_tasks = self.config.classification_tasks[class_tasks_key]
+            seg_fore_loss_dic[class_tasks_key] = 0
+            seg_back_loss_dic[class_tasks_key] = 0
+
+            nclasses = int(classification_tasks['classes']) #number of classes
+            rec_channels = classification_tasks['rec_channels'] #list with channels to reconstruct
+            nrec_channels = len(rec_channels)
+
+            ncomponents = np.array(classification_tasks['ncomponents'])
+            if len(out.shape)<= 4:
+                out_seg = torch.nn.Softmax(dim=1)(out[:, ix:ix + np.sum(ncomponents),...]) # batch_size x channels x h x w
+            else:
+                out_seg = torch.nn.Softmax(dim=2)(out[:, :, ix:ix + np.sum(ncomponents), ...])  #predictions x batch_size x channels x h x w
+                out_seg = torch.mean(out_seg, 0)
+            ix += np.sum(ncomponents)
+
+            ## foreground scribbles loss for each class ##
+            ix_seg = 0
+            for ix_class in range(nclasses):
+                out_seg_class = torch.sum(out_seg[:, ix_seg:ix_seg + ncomponents[ix_class], ...], 1) #batch_size x h x w
+                scribbles_class = scribble[:, ix_scribbles, ...] #batch_size x h x w
+                num_scribbles = torch.sum(scribbles_class, [1, 2]) #batch_size
+                ix_scribbles += 1
+                ix_seg += ncomponents[ix_class]
+
+                if torch.sum(num_scribbles) > 0:
+                    seg_class_loss = self.criterio_seg(out_seg_class, scribbles_class) * scribbles_class #batch_size x h x w
+                    seg_class_loss = torch.sum(seg_class_loss, [1, 2]) / torch.max(num_scribbles,torch.ones_like(num_scribbles)) #batch_size
+                    seg_class_loss = torch.sum(seg_class_loss)/torch.sum(torch.min(num_scribbles,torch.ones_like(num_scribbles)))
+                    seg_fore_loss_dic[class_tasks_key] += classification_tasks['weight_classes'][ix_class]*seg_class_loss #mean of nonzero nscribbles across batch samples
+
+            total_loss['seg_fore'] += seg_fore_loss_dic[class_tasks_key] * self.config.weight_tasks[class_tasks_key]
+
+
+            ## background ##
+            out_seg_back = torch.sum(out_seg[:, ix_seg:ix_seg + ncomponents[ix_class+1], ...],1)  # batch_size x h x w
+            scribbles_back = scribble[:, ix_scribbles, ...]  # batch_size x h x w
+            num_scribbles = torch.sum(scribbles_back, [1, 2])  # batch_size
+            ix_scribbles += 1
+            ix_seg += ncomponents[ix_class+1]
+
+            if torch.sum(num_scribbles) > 0:
+                seg_back_loss = self.criterio_seg(out_seg_back, scribbles_back) * scribbles_back  # batch_size x h x w
+                seg_back_loss = torch.sum(seg_back_loss, [1, 2]) / torch.max(num_scribbles,torch.ones_like(num_scribbles))  # batch_size
+                seg_back_loss_dic[class_tasks_key] += torch.sum(seg_back_loss) / torch.sum(torch.min(num_scribbles,torch.ones_like(num_scribbles)))  # mean of nonzero nscribbles across batch samples
+
+            total_loss['seg_back'] += seg_back_loss_dic[class_tasks_key] * self.config.weight_tasks[class_tasks_key]
+
+
+            ## Regularization loss (MS penalty)
+            if self.criterio_reg is not None:
+                ## Regularization
+                reg_loss_dic[class_tasks_key] = self.criterio_reg(out_seg)
+                total_loss['reg'] += reg_loss_dic[class_tasks_key] * self.config.weight_tasks[class_tasks_key]
+
+
+            ## Reconstruction ##
+            rec_loss_dic[class_tasks_key] = 0
+
+            if (nrec_channels > 0) & (self.criterio_rec is not None):
+                for ix_ch in range(nrec_channels):
+                    ch = rec_channels[ix_ch]
+                    # channel to reconstruct for this class object
+                    num_mask = torch.sum(1 - mask[:, ch, :, :], [1, 2]) #size batch
+
+                    if self.config.mean:  # mean values per fore+back
+                        # mean_values = torch.mean(out[:, ix:ix + np.sum(ncomponents), ...], [2, 3]) #batch x (nfore*nclasses + nback)
+                        if len(out.shape) <= 4:
+                            mean_values = torch.sum(out[:, ix: ix + np.sum(ncomponents), ...]*out_seg, [2, 3])  # batch x (nfore*nclasses + nback)
+                        else:
+                            mean_values = torch.sum(torch.mean(out[:, :, ix: ix + np.sum(ncomponents), ...],0)*out_seg, [2, 3])  # prediction x batch x (nfore*nclasses + nback)
+                        mean_values = mean_values/torch.sum(out_seg,[2, 3])
+                        ix += np.sum(ncomponents)
+                    else:
+                        mean_values = torch.zeros([out_seg.shape[0], np.sum(ncomponents)])
+                        mean_values = mean_values.to(self.config.device)
+                    mean_values = torch.unsqueeze(torch.unsqueeze(mean_values, -1), -1)
+
+                    if self.config.std:  # logstd values per fore+back
+                        # std_values = torch.mean(out[:, ix:ix + np.sum(ncomponents), ...],[2, 3])  # assume this is log(std)
+                        if len(out.shape) <= 4:
+                            std_values = torch.sum(out[:, ix: ix + np.sum(ncomponents), ...]*out_seg, [2, 3])
+                        else:
+                            std_values = torch.sum(torch.mean(out[:, :, ix: ix + np.sum(ncomponents), ...],0)*out_seg, [2, 3])
+                        std_values = std_values/torch.sum(out_seg,[2, 3])
+                        ix += np.sum(ncomponents)
+                    else:
+                        std_values = torch.zeros([out_seg.shape[0], np.sum(ncomponents)])  # assume this is log(std)
+                        std_values = std_values.to(self.config.device)
+                    std_values = torch.unsqueeze(torch.unsqueeze(std_values, -1), -1)
+
+                    mean_x = torch.sum(mean_values * out_seg, 1)
+                    std_x = torch.sum(std_values * out_seg, 1)
+                    rec_x = self.criterio_rec(input[:, ch, ...], mean=mean_x, logstd=std_x) * (1 - mask[:, ch, :, :])
+                    rec_loss_dic[class_tasks_key] += torch.mean(torch.sum(rec_x, [1, 2]) / num_mask) * classification_tasks['weight_rec_channels'][ix_ch] #average over al channels
+
+                total_loss['rec'] += rec_loss_dic[class_tasks_key] * self.config.weight_tasks[class_tasks_key]
+
+        ## Additional losses for reference
+        total_loss['seg_fore_classes'] = seg_fore_loss_dic
+        total_loss['seg_back_classes'] = seg_back_loss_dic
+        total_loss['rec_channels'] = rec_loss_dic
+        total_loss['reg_classes'] = reg_loss_dic
+
+        return total_loss
+
+
+
 class seglosses(nn.Module):
 
     def __init__(self, type_loss = 'L2', reduction = None):
