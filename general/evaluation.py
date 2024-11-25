e674c2e25e6a6e9d4a964cee0516ed1103b5aa67
Gunjan Shrivastava <gunjan.sh123@gmail.com>
2024-11-25 05:32:23 -0500

    Add evaluation metrics for prediction labels directly

--- general/evaluation.py
+++ general/evaluation.py
@@ -1,7 +1,20 @@
 import numpy as np
 import pandas as pd
+import skimage
 from scipy import ndimage
 from sklearn.metrics import roc_auc_score, jaccard_score
+from general.outlines import dilate_masks
+from general.metrics_stardist import matching_dataset
+
+
+def safe_division(x, y, eps=1e-10):
+    """computes a safe divide which returns 0 if y is zero"""
+    if np.isscalar(x) and np.isscalar(y):
+        return x/y if np.abs(y)>eps else 0.0
+    else:
+        out = np.zeros(np.broadcast(x,y).shape, np.float32)
+        np.divide(x,y, out=out, where=np.abs(y)>eps)
+        return out
 
 
 def pixel_sharing_bipartite(lab1, lab2):
@@ -12,24 +25,37 @@ def pixel_sharing_bipartite(lab1, lab2):
     return psg
 
 
-def get_performance(label_gt, y_pred, threshold=0.5, iou_threshold=0.5):
+def get_performance(label_gt, y_pred, threshold=0.5, iou_threshold=0.5, dilate=False):
     pred_thresholded = y_pred > threshold
-    labels_pred, _ = ndimage.label(pred_thresholded)
+    label_pred, _ = ndimage.label(pred_thresholded)
+    if dilate:
+        # min_size = 2 
+        # dilation iter = 1
+        label_pred = skimage.morphology.remove_small_objects(label_pred, min_size=5)
+        label_pred = dilate_masks(label_pred, n_iter=1)
+
+    return get_performance_labels(label_gt, label_pred, iou_threshold)
+
+
+def get_performance_labels(label_gt, label_pred, iou_threshold=0.5):
 
     # print("impartial_evaluation_debug_label_gt", label_gt.shape)
     # print("impartial_evaluation_debug_y_pred", y_pred.shape)
 
     # auc
-    auc = roc_auc_score((label_gt > 0).flatten(), y_pred.flatten())
+    auc = 0.0
+    # auc = roc_auc_score((label_gt > 0).flatten(), y_pred.flatten())
     # jaccard or iou semantic level
-    jacc = jaccard_score((label_gt > 0).flatten(), pred_thresholded.flatten())
+    jacc = jaccard_score((label_gt > 0).flatten(), (label_pred > 0).flatten())
 
     # instance: IoU & Dice
-    psg = pixel_sharing_bipartite(label_gt, labels_pred)
+    psg = pixel_sharing_bipartite(label_gt, label_pred)
     fn = np.sum(psg, 1, keepdims=True) - psg
     fp = np.sum(psg, 0, keepdims=True) - psg
-    IoU = psg / (fp + fn + psg)
-    Dice = 2 * psg / (fp + fn + 2 * psg)
+    # IoU = psg / (fp + fn + psg)
+    IoU = safe_division(psg, (fp + fn + psg))
+    # Dice = 2 * psg / (fp + fn + 2 * psg)
+    Dice = safe_division(2 * psg, (fp + fn + 2 * psg))
 
     # Precision (AP)
     matching = IoU > iou_threshold
@@ -38,17 +64,32 @@ def get_performance(label_gt, y_pred, threshold=0.5, iou_threshold=0.5):
     assert matching.sum(0).max() < 2
     assert matching.sum(1).max() < 2
     n_gt = len(set(np.unique(label_gt)) - {0})
-    n_hyp = len(set(np.unique(labels_pred)) - {0})
+    n_hyp = len(set(np.unique(label_pred)) - {0})
     n_matched = matching.sum()
 
-    precision = n_matched / (n_gt + n_hyp - n_matched)
+    # precision = n_matched / (n_gt + n_hyp - n_matched)
+    precision = safe_division(n_matched, (n_gt + n_hyp - n_matched))
 
-    metrics = {'auc': auc,
+    metrics = {
+               'auc': auc,
                'Jacc': jacc,
                'mIoU': np.mean(np.max(IoU, axis=1)),
                'mDice': np.mean(np.max(Dice, axis=1)),
-               'AP': precision}
+               'AP': precision
+               }
+    
+    # stardist based metrics
+    metrics_sd_label = matching_dataset(label_gt, label_pred, iou_threshold, show_progress=False)
+    metrics_sd = { 
+                'precision_sd': metrics_sd_label.precision, 
+                'recall_sd' : metrics_sd_label.recall, 
+                'accuracy_sd': metrics_sd_label.accuracy, 
+                'f1_sd': metrics_sd_label.f1,
+                'panoptic_quality': metrics_sd_label.panoptic_quality 
+                }
 
+    metrics.update(metrics_sd)
+    
     return metrics
 
 
